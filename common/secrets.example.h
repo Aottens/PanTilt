9ee7c165b469b8aa0621676e72a6960a219977a2
Aottens <52961612+Aottens@users.noreply.github.com>
2025-06-10 09:42:29 +0200

    Add MAC placeholders to secrets example

--- common/secrets.example.h
+++ common/secrets.example.h
@@ -6,3 +6,7 @@ static const uint8_t WIFI_KEY[16] = {
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 };
+
+// Replace with the MAC addresses of your controller and head boards
+static const uint8_t CONTROLLER_MAC[6] = {0, 0, 0, 0, 0, 0};
+static const uint8_t HEAD_MAC[6] = {0, 0, 0, 0, 0, 0};
